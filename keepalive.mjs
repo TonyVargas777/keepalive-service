aac54e7642c35644ca8280653ca7f52ca863acf5
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2025-02-12 18:16:17 +0100

    add juegos_url

--- keepalive.mjs
+++ keepalive.mjs
@@ -2,27 +2,34 @@ import express from 'express';
 import fetch from 'node-fetch';
 
 const app = express();
-const PORT = process.env.PORT || 10000; // Render asignará automáticamente un puerto
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
     res.send('Keepalive service is running');
 });
 
-// Función para enviar solicitudes periódicas
+// Lista de URLs de las bases de datos o servicios que deseas mantener activos
+const urls = [
+    'https://basedatosrender.onrender.com',
+    'https://los-juegos-de-mi-vida.onrender.com'
+];
+
+// Función para enviar solicitudes periódicas a todas las URLs
 const keepAlive = async () => {
-    try {
-        console.log('Enviando solicitud a https://basedatosrender.onrender.com...');
-        const response = await fetch('https://basedatosrender.onrender.com');
-        console.log('Solicitud exitosa', await response.text());
-    } catch (error) {
-        console.error('Error al enviar la solicitud:', error);
+    for (const url of urls) {
+        try {
+            console.log(`Enviando solicitud a ${url}...`);
+            const response = await fetch(url);
+            console.log(`Solicitud exitosa a ${url}:`, await response.text());
+        } catch (error) {
+            console.error(`Error al enviar la solicitud a ${url}:`, error);
+        }
     }
 };
 
 // Llamar a la función cada 5 minutos
 setInterval(keepAlive, 5 * 60 * 1000);
 
-// Iniciar el servidor para satisfacer Render
 app.listen(PORT, () => {
     console.log(`Keepalive service started on port ${PORT}`);
 });
